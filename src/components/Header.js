2ab517f8174f6de934e66c812e9b50e6d5cc5a9e
Roger Campbell II <rc1336@gmail.com>
2019-04-21 20:21:25 -0400

    npm i const ghpages this is a dependency that help deploy to gitpages.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,19 @@ import React from 'react'
 import Footer from './Footer'
 import avatar from '../assets/images/GA_02_-83.jpg'
 
-// import './Header.css'
+import './Header.css'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    {/* <div className="image-cropper"> */}
-                    <a href="#" className="image avatar profile-pic"><img src={avatar} alt="Roger Campbell II" /></a>
-                    {/* </div> */}
-                    <h1>Roger Campbell II </h1>
-                    <h1>Software Engineer</h1>
-                    <p>Washington DC</p>
+                    <div className="test">
+                        <a href="#" className="image avatar"><img src={avatar} alt="Roger Campbell II" /></a>
+                        <h1>Roger Campbell II </h1>
+                        <h1>Software Engineer</h1>
+                        <p>Washington DC</p>
+                    </div>
                     {/* <br />
                     <h1>Software Engineer</h1> */}
                     {/* <h1><strong>I am Roger Campbell</strong>, a mission driven Software Engineer equipped with the insight to thrive under pressure and tackle complex problems.
