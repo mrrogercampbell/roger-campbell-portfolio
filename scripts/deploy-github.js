f6bca60f1a34189393d4ecb47cbfe6cf21508870
Roger Campbell II <rc1336@gmail.com>
2019-04-21 20:44:17 -0400

    Seeing if I can figure out how to deploy this site. Need to finish understanding this deploy-github.js file.

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -6,6 +6,7 @@ ghpages.publish(
     {
         branch: 'master',
         repo: 'https://github.com/flexdinesh/flexdinesh.github.io.git',
+        // repo: 'git@github.com:rc1336/roger-campbell-portfolio.git',
     },
     () => {
         console.log('Deploy Complete!')
