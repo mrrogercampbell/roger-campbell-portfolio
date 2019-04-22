12dc5758c26aeef78e584262a04b265a2ddbfd71
Roger Campbell II <rc1336@gmail.com>
2019-04-21 21:49:09 -0400

    Just ran the github script which should have created a static build that will push and render my new portfolio to my github pages. Next Steps: Need to troubleshoot how to switch my old github pages repo to this new repo

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -5,8 +5,8 @@ ghpages.publish(
     'public',
     {
         branch: 'master',
-        repo: 'https://github.com/flexdinesh/flexdinesh.github.io.git',
-        // repo: 'git@github.com:rc1336/roger-campbell-portfolio.git',
+        // repo: 'https://github.com/flexdinesh/flexdinesh.github.io.git',
+        repo: 'git@github.com:rc1336/roger-campbell-portfolio.git',
     },
     () => {
         console.log('Deploy Complete!')
